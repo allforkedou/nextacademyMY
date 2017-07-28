af1d907f052ccbf2f8a9b594f494ba131d51c34f
Wee Jing Heng <wheng3@illinois.edu>
2017-07-28 13:47:56 +0800

    .

---
+++ day1summary.rb
@@ -0,0 +1,9 @@
+def user_details(name, occupation)
+  "Hi! My name is #{name} and I am a #{occupation}."
+end
+
+######
+# Call the method
+user_name = "Glo"
+user_occupation = "Lecturer"
+p user_details(user_name, user_occupation)
