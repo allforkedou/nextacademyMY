92fe049cc1afb93b2c08e42f6f7f95657db2e600
Wee Jing Heng <wheng3@illinois.edu>
2017-07-31 14:18:18 +0800

    generating_anagrams

---
+++ generating_anagrams.rb
@@ -0,0 +1,18 @@
+def canonical(word)
+  w = word.downcase.chars.sort.join
+  return w
+end
+
+def is_anagram?(word_one, word_two)
+		return canonical(word_one)==canonical(word_two)
+end
+
+def anagrams_for(word, arr_words)
+	arr = []
+	for i in 0..arr_words.length-1
+		if(is_anagram?(word, arr_words[i]))
+			arr << arr_words[i]
+		end
+	end
+	return arr
+end
