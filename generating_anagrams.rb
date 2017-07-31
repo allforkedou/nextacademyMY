1df7cb2912a4e0b02bf2d194893827c8898188e5
Wee Jing Heng <wheng3@illinois.edu>
2017-07-31 14:39:46 +0800

    generating angrams with hash

--- generating_anagrams.rb
+++ generating_anagrams.rb
@@ -8,11 +8,17 @@ def is_anagram?(word_one, word_two)
 end
 
 def anagrams_for(word, arr_words)
+	canon = canonical(word)
+	hash = {}
 	arr = []
+	hash[canon] = arr
 	for i in 0..arr_words.length-1
 		if(is_anagram?(word, arr_words[i]))
-			arr << arr_words[i]
+			hash[canon] << arr_words[i]
 		end
 	end
-	return arr
+	return hash[canon]
 end
+
+#Testcase
+p anagrams_for('happy', ['pyahp','yapph','ppap','HAPYP'])
