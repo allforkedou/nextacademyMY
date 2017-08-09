69d5122ed97f737db1acd17d9fe6b6fc8d690e54
Wee Jing Heng <wheng3@illinois.edu>
2017-08-09 13:10:17 +0800

    sudoku_2

--- day5/boggle_2_testing_4.rb
+++ day5/boggle_2_testing_4.rb
@@ -1,30 +1,33 @@
-#This is the version that can find all words but will use repeated dice
-
+#Boggle board test version with board being
+  #P P A P
+  #A B C D
+  #L M A O
+  #A S I A
 require "matrix"
 class BoggleBoard
-    @@dice = [
-              [%w{A A E E G N},
-              %w{E L R T T Y},
-              %w{A O O T T W},
-              %w{A B B J O O}],
-              [%w{E H R T V W},
-              %w{C I M O T U},
-              %w{D I S T T Y},
-              %w{E I O S S T}],
-              [%w{D E L R V Y},
-              %w{A C H O P S},
-              %w{H I M N Q U},
-              %w{E E I N S U}],
-              [%w{E E G H N W},
-              %w{A F F K P S},
-              %w{H L N N R Z},
-              %w{D E I L R X}]
-            ]
+    # @@dice = [
+    #           [%w{A A E E G N},
+    #           %w{E L R T T Y},
+    #           %w{A O O T T W},
+    #           %w{A B B J O O}],
+    #           [%w{E H R T V W},
+    #           %w{C I M O T U},
+    #           %w{D I S T T Y},
+    #           %w{E I O S S T}],
+    #           [%w{D E L R V Y},
+    #           %w{A C H O P S},
+    #           %w{H I M N Q U},
+    #           %w{E E I N S U}],
+    #           [%w{E E G H N W},
+    #           %w{A F F K P S},
+    #           %w{H L N N R Z},
+    #           %w{D E I L R X}]
+    #         ]
 
   def initialize
     @board = Array.new(4){Array.new(4)} #Would be easier to just make a 16x1 matrix
-    @travelled = []
-    @chars_hash = {}
+    @hash = {} #The hash table of position vectors of characters
+    @visited = [] #The array of position vectors visited
   end
 
   def shake!
@@ -50,69 +53,111 @@ class BoggleBoard
     end
   end
 
-  def check_neighbors(current_char,word_remaining)
-    if(word_remaining == "")
+  def include?(word)
+    create_hash(word)
+    #if the boggle doesn't have every alphabet, don't even bother to call helper function and return false
+    if !include_every_alphabet?(word)
+      return false
+    end
+    if include_helper(word, check_hash(word[0]))
+      print_visited_path
       return true
     else
-      # p current_char, word_remaining
-      for i in -1..1
-        for j in -1..1
-          for k in 0..check_hash(current_char).length-1
-            if (check_hash(word_remaining[0])- @travelled).include?(check_hash(current_char)[k]+Vector[i,j])
-                @travelled << check_hash(current_char)[k]
-                p @travelled
-              return check_neighbors(word_remaining[0], word_remaining[1..-1])
-            end
-          end
-        end
-      end
       return false
     end
   end
 
-  # def is_adjacent(vector)
-  #   for i in -1..1
-  #     for j in -1..1
-  #       if(@travelled[-1]+Vector[i,j] == vector)
-  #         return true
-  #       else
-  #         #do nothing
-  #       end
-  #     end
-  #   end
-  #   return false
-  # end
+  #Where the magic happens
+  def include_helper(word, restricted_hash)
+    #if there is only an alphabet to be found and is on boggle board, return true
+    hash_w0 = restricted_hash
+    hash_w1 = check_hash(word[1])
 
-  def check_hash(char)
-    if(@chars_hash[char].nil?)
-      @chars_hash[char] = []
-      for i in 0..3
-        for j in 0..3
-          if (@board[i][j] == char)
-            @chars_hash[char] << Vector[i,j]
+    for k in 0..hash_w0.length-1
+      @visited << hash_w0[k]
+      print_visited_path
+      #This is the point you should put print_visited_path to see the visiting process
+      if (word.length==1)
+        return true
+      end
+      valid, valid_i_j = is_valid(hash_w0[k], hash_w1)
+      if valid
+        if include_helper(word[1..-1],valid_i_j)
+          return true
+        else
+          @visited.pop
+        end
+      else
+        @visited.pop
+      end
+    end
+    return false
+  end
+
+  #Check if the boggle board has all the possible alphabets
+  def include_every_alphabet?(word)
+    for i in 0..word.length-1
+      return false if (check_hash(word[i])==[])
+    end
+    return true
+  end
+
+  #Create hash table of position vectors for every character in the word
+  def create_hash(word)
+    for k in 0..word.length-1
+      if(@hash[word[k]].nil?)
+        @hash[word[k]] = []
+        for i in 0..3
+          for j in 0..3
+            @hash[word[k]] << Vector[i,j] if (@board[i][j] == word[k])
           end
         end
+        #Can implement include_every_alphabet? here but will look very clumsy
       end
+    end
+  end
+
+  #Check and return the hash table of the character
+  def check_hash(char)
+    if char.nil?
+      return nil
     else
-      #do nothing
+      return @hash[char]
     end
-    return @chars_hash[char]
   end
 
-  def include? (word)
-    for i in 0..word.length-1
-        if (check_hash(word[i])==[])
-          return false
+  #Check if the current char vector can visit the next char and return the possible Vectors [i,j]
+  def is_valid(vector, hash_w1)
+    valid_i_j = []
+    for i in -1..1
+      for j in -1..1
+        if(hash_w1-@visited).include?(vector+Vector[i,j])
+          valid_i_j << vector+Vector[i,j]
         end
+      end
     end
-    return check_neighbors(word[0],word[1..-1])
+    if (valid_i_j == [])
+      return false, valid_i_j
+    else
+      return true, valid_i_j
+    end
   end
 
-end
+  def print_visited_path
+    @visited.each do |vector|
+      print vector.to_s.delete('Vector') + '->'
+    end
+    puts
+  end
 
-
+end #This is Class' end! DO NOT DELETE!
 
   board = BoggleBoard.new
   board.shake!
   puts board
-  puts board.include?('PPAP')
+
+  #Testcases
+  # puts board.include?('PPAP')
+  puts board.include?('PPAPP')
+  # puts board.include?('PALASIAA')
+  # puts board.include?('B')
