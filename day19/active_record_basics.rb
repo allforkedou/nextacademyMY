12def5f490d2bdf9eea0de9c90025528f53bde64
Wee Jing Heng <wheng3@illinois.edu>
2017-08-23 10:23:20 +0800

    active_record_basics

--- day19/active_record_basics.rb
+++ day19/active_record_basics.rb
@@ -30,3 +30,28 @@ s3.destroy
 Student.count
 Student.last
 Student.second
+
+#table name is always plural
+#class name is always singular
+
+#Tutorial codes from Lecture
+#irb load 'playground.rb'
+#irb -r ./playground.rb
+#Find by gives only one matched object, where gives array
+#Student.find(11) #11 is the ID
+#Student.find_by(email: "ciara_wolf@towne.io")
+#Student.where(age: 21)
+#Student.where('age <= ?', 21)
+# Student.where(age: 21).count
+#Student.all
+#student = Student.new #This creates new student object not in database yet
+#s2 = Student.find_by(name: 'Kevin')
+# s2.email = 'kevin@handsome.com'
+# s2.save
+# s3 = Student.second
+# s3.update(age: 50, name: 'yusheng', email: 'haha@gmail.com')
+# s4 = Student.find_by(email:'kevin@handsome.com')
+# s4.destroy
+# s4 = Student.find_by(email:'kevin@handsome.com') #nil
+# s4 = Student.where(email:'kevin@handsome.com') #empty array
+# Student.destroy(1) #Assuming you know the id
