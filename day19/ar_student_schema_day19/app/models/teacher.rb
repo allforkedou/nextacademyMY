bf4644856fa5fed60d28f00b5e92be3da3efe328
Wee Jing Heng <wheng3@illinois.edu>
2017-08-24 14:12:00 +0800

    completed_many_to_many_associations_between_student_teacher

---
+++ day19/ar_student_schema_day19/app/models/teacher.rb
@@ -0,0 +1,10 @@
+require_relative '../../db/config'
+
+class Teacher < ActiveRecord::Base
+	has_many :student_teachers
+	has_many :students, :through => :student_teachers
+
+	validates_format_of :email,:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
+	validates :email, uniqueness: true
+	validates_format_of :phone, :with => /\D*(?:\d\D*){10,}/
+end
---
+++ day19/ar_student_schema_day19/db/migrate/201710823141100_create_teachers.rb
@@ -0,0 +1,17 @@
+require_relative '../config'
+
+# this is where you should use an ActiveRecord migration to
+
+class CreateTeachers < ActiveRecord::Migration[5.0]
+  def change
+    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :teachers do |t|
+
+      # add columns that you would need for this table
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.timestamps
+    end
+  end
+end
