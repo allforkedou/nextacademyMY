d1acc02f007201346bc00380be31772d31dc1789
Wee Jing Heng <wheng3@illinois.edu>
2017-08-29 15:08:26 +0800

    inside_out_commit

--- day23/js-events-master/submit-button/index.js
+++ day23/js-events-master/submit-button/index.js
@@ -2,7 +2,6 @@ function getEncryptedWord() {
 
   //get the word input by the user
   var str = document.getElementById("word").value;
-  
   console.log(str); //remember to open your developer's tool in the browswer and open up the console log so that you can see this function in action
 
   //change the first letter of the word to "b"
